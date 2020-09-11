3891eeeae5d93edb0e91f3891a640516f1dea2a0
Gigi Pusztai <pg3193@gmail.com>
2020-09-11 15:07:58 +0200

    boot.s and main.c created

---
+++ src/boot.s
@@ -0,0 +1,5 @@
+/*
+* boot.s - Kernel start location. Also defines multiboot header.
+* Based on Bran's kernel development tutorial file start.asm
+*
+*/
---
+++ src/main.c
@@ -0,0 +1,6 @@
+/*
+* main.c -- defines the C-code kernel entry point, calls initialization routines.
+* Based on JamesM's tutorial
+*/
+
+int main(struct multiboot *mboot_ptr)
